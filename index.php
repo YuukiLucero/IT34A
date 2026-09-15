6935bc162eb1567c67f7135bc901eb29f61e97c8
YuukiLucero <meyklucero@gmail.com>
2026-09-15 13:56:13 +0800

    Added activity login and logout

--- index.php
+++ index.php
@@ -1,66 +1,84 @@
-<?php
-require 'config/config.php';
-require 'config/functions.php';
-
-if (isset($_SESSION['user_id'])) {
-    header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
-    exit;
+<?php 
+require 'config/config.php'; 
+require 'config/functions.php'; 
+ 
+if (isset($_SESSION['user_id'])) { 
+    header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php'); 
+    exit; 
+} 
+ 
+$error = ''; 
+ 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
+    $login = trim($_POST['login'] ?? ''); 
+    $password = $_POST['password'] ?? ''; 
+ 
+    $error = 'Invalid login credentials'; 
+    
+ if($login===''||$password===''){ 
+        $error = 'Invalid login credentials'; 
+        logActivity( 
+            $pdo, 
+            null, 
+            $login, 
+             'login', 
+             'failed', 
+        ); 
+    } else { 
+        if(loginUser($pdo, $login, $password)){ 
+            
+            logActivity( 
+                $pdo, 
+                $_SESSION['user_id'], 
+                $_SESSION['user_email'], 
+                 'login', 
+                 'success', 
+            ); 
+            header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php'); 
+            exit; 
+        }
 }
-
-$error = '';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $login = trim($_POST['login'] ?? '');
-    $password = $_POST['password'] ?? '';
-
-    if (loginUser($pdo, $login, $password)) {
-        header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
-        exit;
-    }
-
-    $error = 'Invalid login credentials';
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-
-<body>
-
-    <?php if ($error): ?>
-        <p><?= htmlspecialchars($error) ?></p>
-    <?php endif; ?>
-
-    <h1>User Login</h1>
-
-    <form method="POST">
-
-        <label>Username or email</label>
-        <input
-            type="text"
-            name="login"
-            required
-        >
-
-        <br>
-
-        <label>Password</label>
-        <input
-            type="password"
-            name="password"
-            required
-        >
-
-        <br>
-
-        <button type="submit">Sign In</button>
-
-    </form>
-
-</body>
-</html>
+} 
+?> 
+ 
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Document</title> 
+</head> 
+<body> 
+ 
+    <?php if ($error): ?> 
+        <p><?= htmlspecialchars($error) ?></p> 
+    <?php endif; ?> 
+ 
+    <h1>User Login</h1> 
+ 
+    <form method="POST"> 
+ 
+        <label>Username or email</label> 
+        <input 
+            type="text" 
+            name="login" 
+            required 
+        > 
+ 
+        <br> 
+ 
+        <label>Password</label> 
+        <input 
+            type="password" 
+            name="password" 
+            required 
+        > 
+ 
+        <br> 
+ 
+        <button type="submit">Sign In</button> 
+ 
+    </form> 
+ 
+</body> 
+</html> 
